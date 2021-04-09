3423988a734e177ac8affc3a24ef1a4fc929332a
JonathanvanWestendorp <mrjonathanvw@gmail.com>
2021-04-09 12:18:47 +0200

    Straighten sequential data

--- dataloader.py
+++ dataloader.py
@@ -3,57 +3,34 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
+# Outermost dates in dataset
+BASE_TIME = pd.Timestamp("2014-02-15")
+END_TIME = pd.Timestamp("2014-06-09")
+
 
 def process(data):
-    # new_data = pd.DataFrame(columns=['id', 'time_start', 'time_end' 'circumplex.arousal', 'circumplex.valence',
-    #                                  'activity', 'screen', 'call', 'sms', 'appCat.builtin', 'appCat.communication'
-    #                                  'appCat.entertainment', 'appCat.finance', 'appCat.game', 'appCat.office'
-    #                                  'appCat.other', 'appCat.social', 'appCat.travel', 'appCat.unknown'
-    #                                  'appCat.utilities', 'appCat.weather', 'mood'])
-    data['time'] = pd.to_datetime(data.time)
-    print(data['id'].nunique())
-    ids = data['id'].unique()
-    features = data[data['variable'] != "mood"]['variable'].unique()
-    for patient_id in ids:
-        patient_data = data[data['id'] == patient_id]
-        for feature in features:
-            patient_feature_data = patient_data[patient_data['variable'] == feature]
-            ## HIER MOETEN WE WAT MEE!
-            print(patient_feature_data.resample('D', on='time', origin=pd.Timestamp('2014-01-01'))['value'].mean())
+    inputs, targets = [], []
+    data.time = pd.to_datetime(data.time)
+    for patient_id in data.id.unique():
+        patient_inputs = []
+        patient_data = data[data.id == patient_id]
+        for feature in data.variable.unique():
+            outer_rows = pd.DataFrame([[patient_id, BASE_TIME, feature, np.nan],
+                                       [patient_id, END_TIME, feature, np.nan]],
+                                      columns=["id", "time", "variable", "value"])
 
-        break
+            patient_feature_data = patient_data[patient_data.variable == feature].append(outer_rows)
 
+            if feature == "mood":
+                patient_mood_grouped = patient_feature_data.resample('D', on='time')['value'].mean()
+                targets.append(patient_mood_grouped.iloc[1::5].tolist())
+            else:
+                patient_feature_grouped = patient_feature_data.resample('5D', on='time')['value'].mean()
+                patient_inputs.append(patient_feature_grouped.tolist())
 
-    targets, inputs = [], []
-
-    # Get targets
-    moods = data[data['variable'] == "mood"]
-    targets = [moods[moods['id'] == patient_id]['value'].tolist() for patient_id in moods['id'].unique()]
-
-    min_target_length = min([len(ls) for ls in targets])
-
-    for i in range(len(targets)):
-        targets[i] = targets[i][:min_target_length]
-
-    # Get inputs
-    features = data[data['variable'] != "mood"]['variable'].unique()
-    for feature in features:
-        intermediate_inputs = []
-        data_per_feature = data[data['variable'] == feature]
-        for patient_id in data['id'].unique():
-            values = data_per_feature[data_per_feature['id'] == patient_id]['value'].tolist()
-            intermediate_inputs.append(values)
-
-        min_length = min([len(ls) for ls in intermediate_inputs])
-        if min_length < min_target_length:
-            continue
-
-        for i in range(len(intermediate_inputs)):
-            intermediate_inputs[i] = intermediate_inputs[i][:min_target_length]
-
-        inputs.append(intermediate_inputs)
-
-    return torch.tensor(inputs).permute(1, 0, 2), torch.tensor(targets)
+        inputs.append(patient_inputs)
+    print(torch.tensor(targets))
+    print(torch.tensor(inputs).shape, torch.tensor(targets).shape)
 
 
 class MoodDataSet(Dataset):
@@ -63,9 +40,13 @@ class MoodDataSet(Dataset):
         self.data = process(self.raw)
 
     def __len__(self):
-        return self.raw['id'].nunique()
+        return self.raw.id.nunique()
 
     def __getitem__(self, idx):
         window_start = np.random.randint(low=0, high=self.data[0].shape[-1] - self.input_length)
         window_end = window_start + self.input_length
         return self.data[0][idx, :, window_start:window_end], self.data[1][idx, window_start:window_end]
+
+if __name__ == "__main__":
+    data = pd.read_csv("dataset_mood_smartphone.csv")
+    print(process(data))
