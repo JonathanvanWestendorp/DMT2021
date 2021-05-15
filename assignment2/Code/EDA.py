8f7ac21fb2d277ed405b97e56e6815e54b0c86d2
Dylan Prins <dylan.prins@supportdesk.nu>
2021-05-15 19:04:26 +0200

    EDA implementation

--- assignment2/Code/EDA.py
+++ assignment2/Code/EDA.py
@@ -7,7 +7,74 @@ from sklearn.decomposition import PCA
 import time
 import mplcursors
 
-def perform_PCA(data, subset=None, norm=None, transform=None):
+def count_bookings(data, subset=None, save=None):
+    if subset:
+        data = data[:subset].copy(deep=True)
+
+    print(f"In total there were {data.srch_id.nunique()} searches done which gave {len(data)} results")
+    print(f"Out of these {data.srch_id.nunique()} searches {len(data[data['booking_bool'] == 1])} properties were booked")
+    print(f"The booked properties had an average star rating of {round(data[data['booking_bool'] == 1].prop_starrating.mean(),2)}")
+    print(f"The properties that weren't booked had an average star rating of {round(data[data['booking_bool'] == 0].prop_starrating.mean(),2)}")
+
+    # Count bookings for each star rating and reorder array based on star rating
+    star_counts = data[data['booking_bool'] == 1].prop_starrating.value_counts().sort_index()
+    sns.barplot(x=star_counts.index, y=star_counts.values)
+    plt.title("Booked properties per star rating")
+    plt.xlabel("Star rating (0 means no rating available)")
+    plt.ylabel("Number of bookings")
+
+    if save:
+        plt.savefig("bookingcounts", dpi=300)
+
+    plt.show()
+
+def average_price(data, subset=None, save=None):
+    if subset:
+        data = data[:subset].copy(deep=True)
+
+    countries = np.sort(data[data['booking_bool'] == 1].prop_country_id.unique())
+    mean_prices = np.array(data[data['booking_bool'] == 1].groupby("prop_country_id").mean()["price_usd"])
+    indices = [i for i, elem in enumerate(mean_prices) if elem >= 1000]
+
+    outliers = mean_prices[indices]
+    outliers_countries = countries[indices]
+
+    mean_prices = mean_prices[mean_prices < 1000]
+    countries = np.delete(countries, indices)
+    plt.scatter(countries, mean_prices)
+    plt.plot(countries, mean_prices)
+    plt.xlabel("Country ID")
+    plt.ylabel("Mean price in USD")
+    plt.title("Mean price of booked properties per country")
+
+    if save:
+        plt.savefig("prices_per_country", dpi=300)
+    plt.show()
+
+    plt.scatter(outliers_countries, outliers)
+    plt.xlabel("Country ID")
+    plt.ylabel("Mean price in USD")
+    plt.title("Mean price of booked properties per country (outliers)")
+    plt.xticks(outliers_countries)
+
+    if save:
+        plt.savefig("prices_per_country_outliers", dpi=300)
+    plt.show()
+
+def promotion(data, subset = None, save=None):
+    if subset:
+        data = data[:subset].copy(deep=True)
+    countings = data[data['booking_bool'] == 1].promotion_flag.value_counts()
+    sns.barplot(x=countings.index, y=countings.values)
+    plt.xticks([0, 1], ["No Sale", "Sale"])
+    plt.title("Amount of booked properties if in sale or not")
+    plt.ylabel("Amount of booked properties")
+
+    if save:
+        plt.savefig("promotions", dpi=300)
+    plt.show()
+
+def perform_PCA(data, subset=None, norm=None, transform=None, save=None):
     if subset:
         data = data[:subset].copy(deep=True)
 
@@ -16,12 +83,13 @@ def perform_PCA(data, subset=None, norm=None, transform=None):
 
     if transform:
         data = data.T
+        legend = "full"
         hue = data.index.values
         ncolors = len(hue)
-
     else:
         ncolors = data.srch_id.nunique()
         hue = "srch_id"
+        legend = False
 
     pca = PCA(n_components=3)
     pca_result = pca.fit_transform(data)
@@ -35,21 +103,24 @@ def perform_PCA(data, subset=None, norm=None, transform=None):
         hue=hue,
         palette=sns.color_palette("hls", ncolors),
         data=data,
-        legend="full",
+        legend=legend,
         alpha=0.8
     )
-
-    if transform:
-        mplcursors.cursor().connect("add", lambda sel: sel.annotation.set_text(hue[sel.target.index]))
 
     plt.title("PCA")
     plt.xlabel("First component")
     plt.ylabel("Second component")
-    plt.legend(fontsize="x-small")
 
+    if transform:
+        plt.legend(fontsize="x-small")
+        mplcursors.cursor().connect("add", lambda sel: sel.annotation.set_text(hue[sel.target.index]))
+        if save:
+            plt.savefig("PCA_transposed", dpi=300)
+    elif save:
+        plt.savefig("PCA", dpi=300)
     plt.show()
 
-def perform_tsne(data, subset=None, norm=None, transform=None):
+def perform_tsne(data, subset=None, norm=None, transform=None, save=None):
     if subset:
         data = data[:subset].copy(deep=True)
 
@@ -57,11 +128,13 @@ def perform_tsne(data, subset=None, norm=None, transform=None):
         data = (data - data.mean()) / (data.max() - data.min())
 
     if transform:
+        legend = "full"
         data = data.T
         hue = data.index.values
         ncolors = len(hue)
     else:
         ncolors = data.srch_id.nunique()
+        legend = False
         hue = "srch_id"
 
     print(f"Amount of columns/features in dataing set: {len(data.columns.values)}")
@@ -78,13 +151,19 @@ def perform_tsne(data, subset=None, norm=None, transform=None):
         hue=hue,
         palette=sns.color_palette("hls", ncolors),
         data=data,
-        legend="full",
+        legend=legend,
         alpha=0.8)
 
     plt.title("T-SNE")
     plt.xlabel("First component")
     plt.ylabel("Second component")
-    plt.legend(fontsize="x-small")
+
+    if transform:
+        plt.legend(fontsize="x-small")
+        if save:
+            plt.savefig("TSNE_transposed", dpi=300)
+    elif save:
+        plt.savefig("TSNE", dpi=300)
     plt.show()
 
 if __name__ == "__main__":
@@ -92,5 +171,12 @@ if __name__ == "__main__":
     train = train.fillna(0)
     del train["date_time"]
 
-    #perform_tsne(train, subset=20000, norm=True, transform=True)
-    perform_PCA(train, subset=10000, norm=True, transform=True)
+    count_bookings(train, save=True)
+    average_price(train, save=True)
+    promotion(train, save=True)
+
+    perform_tsne(train, subset=20000, norm=True, transform=False, save=True)
+    perform_PCA(train, subset=20000, norm=True, transform=False, save=True)
+
+    perform_tsne(train, subset=20000, norm=True, transform=True, save=True)
+    perform_PCA(train, subset=20000, norm=True, transform=True, save=True)
